bb90206d3bc8f88378f4df47818f1ee29e8b6ac1
lordlokion <akhilsharma870@gmail.com>
2023-12-19 17:45:11 +0530

    updated posts

--- components/Posts.vue
+++ components/Posts.vue
@@ -5,10 +5,10 @@
           <h2 class="mb-4 font-heading font-semibold text-gray-900 text-4xl sm:text-7xl">Articles</h2>
         </div>
         <div class="flex flex-wrap -m-9">
-          <div v-for="(post, index) in latestPosts" :key="index" class="w-full md:w-1/3 p-9">
+          <div v-for="(post, index) in latestPosts" :key="index" class="w-full md:w-1/3 p-4">
             <a :href="post.url" class="group">
-              <div class="group flex flex-col mb-5 overflow-hidden rounded-xl aspect-w-3 aspect-h-2 h-64">
-                <img :src="post.image" class="object-cover w-full h-full transform group-hover:scale-110 transition ease-out duration-500 rounded-xl" alt="">
+              <div class="group flex flex-col mb-5 overflow-hidden rounded-xl aspect-w-4 w-80 aspect-h-2 h-40">
+                <img :src="post.image" class="object-fill w-full h-full transform group-hover:scale-110 transition ease-out duration-500 rounded-xl" alt="">
               </div>
               <p class="mb-4 font-heading font-medium text-xl text-gray-900 group-hover:underline">{{ post.title }}</p>
               <h2 class="font-heading font-medium text-xs uppercase text-gray-500 tracking-px">{{ post.category }}</h2>
